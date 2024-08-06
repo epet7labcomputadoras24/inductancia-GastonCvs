79db53a93034a42528610395f9f5e692f01118a3
GastonCvs <cuevasgaston30@gmail.com>
2024-08-06 18:55:43 -0300

    boca yo te amo

---
+++ tarea1/Program.vb
@@ -0,0 +1,18 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim XL As Single
+        Dim L As Single
+        Dim F As Single
+        Dim PI As Single
+        PI = 3.1415
+        Console.Write("De el valor de su inductancia: ")
+        L = Console.ReadLine()
+        Console.Write("De el valor de su frecuencia: ")
+        F = Console.ReadLine()
+        XL = (2 * PI * F * L)
+        Console.Write("El valor de su XL es de: " & XL)
+        Console.ReadLine()
+    End Sub
+End Module
